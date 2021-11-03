851605f72d8e4f1832321058183c4ffcde3a384b
Suraj vishwakarma <90332766+vishwakarmasuraj@users.noreply.github.com>
2021-11-03 17:42:08 +0530

    at 17 : 41 pm
    just committed

--- router/userRouter.js
+++ router/userRouter.js
@@ -6,8 +6,8 @@ const userValidationRules = require('../middleware/userValidationRule')
 const userValid = require('../middleware/valid')
 const userController = require('../controller/user')
 
-router.post('/signup', userValidationRules.userValidationRule(), userValid.validate, userController.userSignup)
-router.post('/login', userController.userLogin)
+router.post('/user-signup', userValidationRules.userValidationRule(), userValid.validate, userController.userSignup)
+router.post('/user-login', userController.userLogin)
 router.get('/get-user', tokenAuth.verifyToken, userController.getUser)
 router.delete('/truncate', userController.truncate)
 
